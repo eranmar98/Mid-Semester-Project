01e868c3eefd863744f2107efb403d7662be0693
eranmar98 <maromeran8@gmail.com>
2025-04-23 21:43:12 +0300

    Bingo Updates
    Displaying the bingo board in JS

---
+++ scripts/bingo.js
@@ -0,0 +1,31 @@
+function init() {
+  let randomNumbers = [];
+
+  // ----- Generate 49 unique random numbers -----
+  for (let i = 0; i < 49; i++) {
+    // Generate a random number between 1 and 100
+    let randomNumber = Math.floor(Math.random() * 100) + 1;
+
+    if (!randomNumbers.includes(randomNumber)) { // check if the number is already in the array
+      // if not, add it to the array
+      randomNumbers.push(randomNumber); 
+    } else {
+      // if the number is already in the array, decrement i to repeat the iteration
+      i--;
+    }
+  }
+
+  // ----- Render the Bingo table -----
+  let table = document.getElementById('bingo-table');// get the table element
+  let index = 0;
+  for (let row = 0; row < 7; row++) {
+    let tr = document.createElement('tr');
+    for (let col = 0; col < 7; col++) {
+      let td = document.createElement('td');
+      td.innerHTML = randomNumbers[index];
+      tr.appendChild(td);
+      index++;
+    }
+    table.appendChild(tr);
+  }
+}
